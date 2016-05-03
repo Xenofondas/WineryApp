24c1b02d71b6437111e7b58993bbf3fd8681ff26
Fontas Amourgis <amourgif@hotmail.com>
2016-05-03 20:34:12 +0300

    LoginPage

--- WineryApp/WineryApp/App.cs
+++ WineryApp/WineryApp/App.cs
@@ -12,19 +12,7 @@ namespace WineryApp
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
